5dd978b3919d080773ee9fb614ed64834e3ba1d2
RalfRick47 <raphaelriquet@free.fr>
2024-09-03 11:05:03 +0200

    ajout du script

---
+++ systeme/script.sh
@@ -0,0 +1,9 @@
+echo 'c/Users/Ralf Rick/eval_git_tssr-fad-24-02'
+read directory
+echo 'projet-delta'
+read projet
+cd $directory
+mkdir $projet
+cd $projet
+touch index.html style.css readme.md
+echo 'le projet a été ajjouté'
